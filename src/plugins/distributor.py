cea8c7cf8ad98284045cb8b5abca5f24194b69e8
Particle_G <particle_g@outlook.com>
2021-06-03 04:40:43 +0800

    - Update sourcecodes

--- src/plugins/distributor.py
+++ src/plugins/distributor.py
@@ -1,12 +1,20 @@
 import os
 from nonebot import on_command, require, get_bots
+from nonebot.config import Config
 from nonebot.rule import to_me
 from nonebot.adapters import Bot, Event
 from nonebot.typing import T_State
 from src.configs import *
 
-delete_file = on_command("del", rule=to_me(), priority=5)
-sync_file = on_command("sync", rule=to_me(), priority=10)
+
+async def admin_and_to_me(bot: Bot, event: Event, state: T_State) -> bool:
+    return event.is_tome() and event.get_user_id() in SUPERUSERS
+
+
+delete_file = on_command("del", rule=admin_and_to_me, priority=5)
+sync_file = on_command("sync", rule=admin_and_to_me, priority=10)
+broadcast = on_command("broadcast", rule=admin_and_to_me, priority=10)
+publish_notice = on_command("notice", rule=admin_and_to_me, priority=5)
 
 
 @delete_file.handle()
@@ -20,6 +28,36 @@ async def handle_first_receive(bot: Bot, event: Event):
             await delete_all(bot, files["folders"])
             await sync_file.send(f"群 {group} 删除文件成功！")
         await delete_file.finish("文件删除成功！")
+
+
+@sync_file.handle()
+async def handle_first_receive(bot: Bot, event: Event):
+    args = str(event.get_message()).strip()
+    if args:
+        await sync_file.reject("sync 指令不支持参数！")
+    else:
+        await check_and_upload(True)
+        await sync_file.finish("文件同步完成！")
+
+
+@broadcast.handle()
+async def handle_first_receive(bot: Bot, event: Event):
+    args = str(event.get_message()).strip()
+    if args:
+        await broadcast_all(bot, args)
+        await broadcast.finish("广播消息发送成功！")
+    else:
+        await broadcast.reject("broadcast 指令需要一个参数！")
+
+
+@publish_notice.handle()
+async def handle_first_receive(bot: Bot, event: Event):
+    args = str(event.get_message()).strip()
+    if args:
+        await notice_all(bot, args)
+        await publish_notice.finish("公告发布成功！")
+    else:
+        await publish_notice.reject("notice 指令需要一个参数！")
 
 
 async def delete_all(bot: Bot, folders):
@@ -38,16 +76,6 @@ async def delete_all(bot: Bot, folders):
                     file_id=file["file_id"],
                     bus_id=file["busid"]
                 )
-
-
-@sync_file.handle()
-async def handle_first_receive(bot: Bot, event: Event, state: T_State):
-    args = str(event.get_message()).strip()
-    if args:
-        await sync_file.reject("sync 指令不支持参数！")
-    else:
-        await check_and_upload(True)
-        await sync_file.finish("文件同步完成！")
 
 
 async def check_and_upload(show_progress):
@@ -100,5 +128,15 @@ async def check_and_upload(show_progress):
                     await sync_file.send(f"群 {group} 同步成功！")
 
 
+async def broadcast_all(bot: Bot, message):
+    for group in DIST_GROUPS:
+        message_id = await bot.call_api("send_group_msg", group_id=group, message=message)
+
+
+async def notice_all(bot: Bot, notice):
+    for group in DIST_GROUPS:
+        await bot.call_api("_send_group_notice", group_id=group, content=notice)
+
+
 scheduler = require('nonebot_plugin_apscheduler').scheduler
-scheduler.add_job(check_and_upload, "cron", hour="4",args=[False])
+scheduler.add_job(check_and_upload, "cron", hour="4", args=[False])
